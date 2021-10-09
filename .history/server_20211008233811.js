f5aa266d0712a36b2340359a472f545a2c7fe646
Mervens Vic <82620500+Mervens@users.noreply.github.com>
2021-10-09 08:46:56 -0400

    Trying to fix live server.js deployment

---
+++ .history/server_20211008233811.js
@@ -0,0 +1,16 @@
+require('./routes/routes')(app);
+
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+}); 
